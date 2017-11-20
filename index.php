8ade3e342d7eda52cb9d9ea9902405d9f7d51e93
Chatchai Srisuk <chatchai.s@tong-siang.com>
2017-11-20 16:42:44 +0700

    1234

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 $channel_token = '7Ief0P/yJzty4DMy+Qw0ybqIQEeT+w38s+iQ+Cf8btBiFQyElMd7a0sKD8JLbsv1RIO0XshvZ44EgTXuk/w31V1THkqSpQtdq7+SurKEK4u6SXX1E4ogT6dt6QcT5BtfXODNoIJuPhtpMLZdGZOGhwdB04t89/1O/w1cDnyilFU='; 
 $channel_secret = 'e44033a80aed821ccecff6ab0df784d4'; 
  
-// Get message from Line API $content = file_get_contents('php://input'); $events = json_decode($content, true); 
+// Get message from Line API 
+$content = file_get_contents('php://input'); $events = json_decode($content, true); 
  
 if (!is_null($events['events'])) {     // Loop through each event     
     foreach ($events['events'] as $event) {         
@@ -21,6 +22,7 @@ if (!is_null($events['events'])) {     // Loop through each event
  
                 // Reply message                       
                  $respMessage = 'Hello, your message is '. $event['message']['text']; 
+                 
                  $httpClient = new CurlHTTPClient($channel_token);                      
                  $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));                       
                  $textMessageBuilder = new TextMessageBuilder($respMessage);                       
