375e8b5757c9b194cfa14b4377668e2927e32add
Chatchai Srisuk <chatchai.s@tong-siang.com>
2017-11-21 15:40:25 +0700

    555

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require_once('./vendor/autoload.php'); 
  session_start();
  if ($_SESSION["active"] == ''){
-    $_SESSION["active"] = "on";
+    $_SESSION["active"] = "onc";
  }
  
 // Namespace 
