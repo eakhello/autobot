890b38aa51957657a8941b416d5539ac6b291bb7
Chatchai Srisuk <chatchai.s@tong-siang.com>
2017-11-25 14:44:42 +0700

    555

--- index.php
+++ index.php
@@ -171,7 +171,7 @@ if (!is_null($events['events'])) {     // Loop through each event
                     //  $respMessage = 'What is your sex? M or F or Img to view image or 12.00,House keeping. to save to db'; 
                     $typeresponse = 'txt';
                 
-                    $sql = "select textout from compare_message where textin = 'a'";
+                    $sql = "select textout from compare_message where textin = '" + strtolower($ask) + "'";
                    $respMessage = $sql;
                    $result = $connection->query($sql);
                     if($result !== false && $result->rowCount() >0) {
